a0d889e647f73d08c7b9f301c4642c76602213cb
jordan lee <leejs198@gmail.com>
2020-11-04 03:33:56 +0900

    Initial commit

---
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,24 @@
+package hello.core.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    public void join() {
+        //when
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        //given
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        //then
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+
+
+}
